53f80f47061d9aa443d4ce856ba2e38f96d093ae
Mori 30R <ghahreman00@gmail.com>
2024-07-18 00:21:08 +0330

    Fix Some Little Bugs on Deploy

--- next/app/_components/PrimaryButton.tsx
+++ next/app/_components/PrimaryButton.tsx
@@ -19,29 +19,30 @@ export default function PrimaryButton({
     function getPadding(size: "small" | "medium" | "big"): string {
         switch (size) {
             case "small":
-                return "5";
+                return "3";
             case "medium":
-                return "7";
+                return "6";
             case "big":
-                return "12";
+                return "10";
         }
     }
 
     function getColor(color: string) {
-        if (color === "green") return "myGreen";
-        else if (color === "white") return "zinc-100";
+        if (color === "green") return "bg-myGreen";
+        else if (color === "white") return "bg-zinc-100";
     }
+
+    const smallStyles = "rounded-full px-5 py-2 hover:ring-myGreen";
+    const notSmallStyles = "rounded-md px-5 py-2 hover:ring-myGreen";
 
     return (
         <Link
             className={`${
-                disabled
-                    ? "bg-transparent hover:ring-1"
-                    : `bg-${getColor(color)}`
-            }  font-semibold ${
+                disabled ? "bg-transparent hover:ring-1" : `${getColor(color)}`
+            } font-semibold ${
                 size === "small" ? "rounded-full" : "rounded-md"
-            } px-${getPadding(size)} py-2  hover:ring-myGreen  ${
-                color === "white" && "text-zinc-900"
+            } px-${getPadding(size)} py-2 hover:ring-myGreen  ${
+                color === "white" && "text-zinc-800"
             }`}
             href={href}
         >
